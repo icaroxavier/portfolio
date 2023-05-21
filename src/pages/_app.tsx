59062ae4519a76ff611ace56c6fe4d30f1c23b35
Ícaro Xavier <icarogabriel999@icloud.com>
2023-05-21 14:44:19 -0300

    Base tailwind project with my assets

--- src/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import '@portfolio/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
