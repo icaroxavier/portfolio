5c0f595e359c6507b7e306d7769a92a85781e515
Ícaro Xavier <icarogabriel999@icloud.com>
2023-05-21 11:13:06 -0300

    google analytics

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -4,6 +4,7 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { ChakraProvider } from '@chakra-ui/react'
 import AlertTemplate from 'react-alert-template-basic'
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import Script from 'next/script'
 
 import GlobalStyle from '../styles/global.style'
 import theme from '../styles/theme'
@@ -20,35 +21,52 @@ const options = {
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
-      <ThemeProvider theme={theme}>
-        <AnimatePresence exitBeforeEnter>
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            variants={{
-              initial: {
-                scale: 0.8,
-                opacity: 0
-              },
-              animate: {
-                scale: 1,
-                opacity: 1,
-                transition: {
-                  delay: 0.4
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-2LMYJ359V4"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2LMYJ359V4', {
+            page_path: window.location.pathname,
+            });
+          `,
+        }}
+    />
+      <ChakraProvider>
+        <ThemeProvider theme={theme}>
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              variants={{
+                initial: {
+                  scale: 0.8,
+                  opacity: 0
+                },
+                animate: {
+                  scale: 1,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.4
+                  }
                 }
-              }
-            }}
-          >
-            <AlertProvider template={AlertTemplate} {...options}>
-              <Component {...pageProps} />
-            </AlertProvider>
-          </motion.div>
-        </AnimatePresence>
-        <GlobalStyle />
-      </ThemeProvider>
-    </ChakraProvider>
+              }}
+            >
+              <AlertProvider template={AlertTemplate} {...options}>
+                <Component {...pageProps} />
+              </AlertProvider>
+            </motion.div>
+          </AnimatePresence>
+          <GlobalStyle />
+        </ThemeProvider>
+      </ChakraProvider>
+    </>
   )
 }
 
