7401b9e7402c1e9c9e7b87d14e330ab80a85a4d1
Ícaro Xavier <icarogabriel999@icloud.com>
2023-05-22 09:21:11 -0300

    Loading in the doggo

---
+++ src/components/voxel-dog-loader.tsx
@@ -0,0 +1,42 @@
+import { forwardRef } from 'react'
+
+export const DogSpinner = () => (
+  <div
+    className="
+    inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em]
+    motion-reduce:animate-[spin_1.5s_linear_infinite]
+    absolute top-[calc(50%-1rem)] left-[calc(50%-1rem)]"
+    role="status"
+  >
+    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+      Loading...
+    </span>
+  </div>
+)
+
+interface DogContainerProps {
+  children: React.ReactNode
+}
+
+export const DogContainer = forwardRef<HTMLDivElement, DogContainerProps>(function dog({ children }, ref) {
+  return(
+    <div
+      ref={ref}
+      className="
+        relative mx-auto
+        h-0 w-full pb-[calc(100%)] -mb-24 -mt-12
+        md:w-[calc(40rem)] md:h-[calc(40rem)] md:-mt-36 md:-mb-48 md:pb-0"
+    >
+      {children}
+    </div>
+)})
+
+const Loader = () => {
+  return (
+    <DogContainer>
+      <DogSpinner />
+    </DogContainer>
+  )
+}
+
+export default Loader
