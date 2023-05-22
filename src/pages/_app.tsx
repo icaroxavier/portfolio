22731e6e133f63b11cc4c8245606656a99036667
Ícaro Xavier <icarogabriel999@icloud.com>
2023-05-21 23:33:17 -0300

    Google analytics and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import { AppProps } from 'next/app';
 
 import '@portfolio/styles/globals.css'
+import Script from 'next/script';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        async
+        strategy='afterInteractive'
+        src="https://www.googletagmanager.com/gtag/js?id=G-1T7PJKTWL8"
+      />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1T7PJKTWL8');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
