6dd82cef82ead63f6325465dd812eb711c58504e
Ícaro Xavier <icarogabriel999@icloud.com>
2023-05-28 14:56:20 -0300

    footer transparent

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,21 +7,21 @@ export function Footer() {
 
     return (
       <footer className="
-        w-full h-16 pt-1 bg-zinc-500 flex flex-col items-center justify-between
-        shadow-lg shadow-zinc-50"
+        w-full h-16 pt-1 bg-transparent flex flex-col items-center justify-between"
+
       >
         <div className="flex gap-3">
           <Link href='https://www.linkedin.com/in/icaroxavier/' target="_blank">
-            <LinkedinLogo size={40} className="cursor-pointer hover:scale-110 transition-all text-zinc-200"/>
+            <LinkedinLogo size={40} className="cursor-pointer hover:scale-110 transition-all text-zinc-70"/>
           </Link>
           <Link href='https://www.github.com/icaroxavier' target="_blank">
-            <GithubLogo size={40} className="cursor-pointer hover:scale-110 transition-all text-zinc-200"/>
+            <GithubLogo size={40} className="cursor-pointer hover:scale-110 transition-all text-zinc-70"/>
           </Link>
           <Link href='https://wa.me/5585998164001?text=Oi,%20tudo%20bem?' target="_blank">
-            <WhatsappLogo size={40} className="cursor-pointer hover:scale-110 transition-all text-zinc-200"/>
+            <WhatsappLogo size={40} className="cursor-pointer hover:scale-110 transition-all text-zinc-70"/>
           </Link>
         </div>
-        <div className="text-zinc-300 text-sm">© {new Date().getFullYear()} {t('footer.copyright')}</div>
+        <div className="text-zinc-500 text-sm">© {new Date().getFullYear()} {t('footer.copyright')}</div>
       </footer>
     );
 }
